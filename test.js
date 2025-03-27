91f55337d2a7f0f7f639096408cef46880120285
tomas0622 <tomas.gomez.restrepo94@gmail.com>
2025-03-26 19:14:29 -0500

    Cree el test y workflow

---
+++ test.js
@@ -0,0 +1,16 @@
+import http from '/k6/http';
+import { check, sleep } from "k6";
+
+export let options = {
+    vus: 10,
+    duration: '10s',
+}
+
+export default function () {
+    let res = http.get('https://test-api.k6.io/public/cocodriles/')
+    check(res, {
+        "Es status 200": (r) => r.status === 200,
+        "Respuesta es menos de 500ms": (r) => r.timings.duration < 500,
+    });
+    sleep(1);
+}
